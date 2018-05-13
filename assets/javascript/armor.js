da786cae23c7858c077fb76a248c7c79023be312
Isaac <isaaclubanko@gmail.com>
2018-05-13 19:02:14 -0400

    added armor names

--- assets/javascript/armor.js
+++ assets/javascript/armor.js
@@ -1,16 +1,20 @@
 var armor = {
     unarmored: {
+        name: "unarmored",
         toAC: 0
     },
     leather: {
+        name: "leather",
         toAC: 1
     },
 
     chain: {
+        name: "chain",
         toAC: 2
     },
 
     plate: {
+        name: "plate",
         toAC: 3,
     }
 }
