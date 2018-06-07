646936fbe54f1f49c4398d83759a41b5e41f7619
Isaac <isaaclubanko@gmail.com>
2018-06-06 22:36:48 -0400

    webpack 4 config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  //mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  devtool: "source-map", 
+  optimization: {
+    // We no not want to minimize our code.
+    //minimize: true
+  },
 };
